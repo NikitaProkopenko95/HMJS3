b2b7bb051fbf02521b109e44a3441b98dcf8f147
Nikita Prokopenko <84684820+NikitaProkopenko95@users.noreply.github.com>
2021-06-07 16:42:05 +0300

    hmjs3

---
+++ script/script.js
@@ -0,0 +1,86 @@
+//Задача 1
+
+/*let num = 1;
+
+while(num <= 50) {
+    console.log(num);
+    num++;
+}
+
+let num1 = 35;
+
+while(num1 >= 8) {
+    console.log(num1);
+    num1--;
+}*/
+
+//задача 2
+
+/*let i = 89;
+
+while(i >= 11) {
+    document.write(i + '<br>');
+    i--;
+}*/
+
+// Задача 3 
+
+/*let sum = 0;
+
+for (let sum1 = 0; sum1 <= 100; sum1++) {
+    sum += sum1;
+    console.log(sum);
+}*/
+
+// Задача 4
+
+/*let w = 0;
+let q = 5;
+
+for ( let g = 1; g <= q; g++) {
+    w = 0;
+    for ( let r = 1; r <= g; r++) {
+    w += r;
+    }
+    console.log(w);
+}*/
+
+// Задача 5 
+
+/*let summa = 6;
+
+while ( summa <= 56 ) {
+    summa++;
+    if (summa % 2 == 0) {
+        console.log(summa);
+    }
+}*/
+
+
+/*for (let e = 8; e <= 56; e++) {
+    if (e % 2 == 0) {
+        console.log(e);
+    }
+}*/
+
+// Задача 6
+
+/*for (let j = 1; j <= 9; j++){   
+    for (let i = 1; i <=9; i++)   
+    console.log(i + "*" + j + "=" +(i*j)); 
+    } */  
+
+// Задача 7
+
+/*let o = 1000;
+
+for (let o = 1000, 
+         z = 0; 
+         o >= 50; 
+         z++) {
+    o = (o/2);
+    console.log(o);
+}*/
+    
+
+
